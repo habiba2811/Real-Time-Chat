38084e64acda2bf14f7f7744d584c899142bd05b
habiba <habiba.hisham2811@gmail.com>
2024-07-31 17:16:10 +0200

    basic setup

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+import dotenv from "dotenv";
+const app = express(); 
+
+dotenv.config();
+const PORT =process.env.PORT || 5000; 
+
+app.get("/",(req,res)=>{ 
+    res.send("hello hi")
+})
+app.listen(PORT,()=> console.log(`server running on port ${PORT}`)); 
