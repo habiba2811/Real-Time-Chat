9dcc932c374600669b24c22e0f57be95bc68ec04
habiba <habiba.hisham2811@gmail.com>
2024-07-31 21:32:07 +0200

    setting db

--- backend/Controllers/authController.js
+++ backend/Controllers/authController.js
@@ -1,11 +1,40 @@
-
+import User from "../models/userModel.js";
+import bcrypt from "bcryptjs";
 export const signup =async(req,res)=>{
     try{
         const {fullName, username,password,confirmPassword,gender}= req.body;
-
-    }
-    catch(error){
-
+      if (password !== confirmPassword){
+        return res.status(400).json({error:"Passwords must match!"})
+      }
+      const user = await User.findOne({username});
+      if(user){
+        return res.status(400).json({error:"User name already exists!"})}
+    
+    //Hash password here
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password,salt);
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
+    const newUser = new User({
+        fullName,
+        username,
+        password: hashedPassword,
+        gender,
+        profilepic: gender === "male"? boyProfilePic : girlProfilePic
+    })
+    if (newUser){await newUser.save();
+    res.status(201).json({
+        _id : newUser._id,
+        fullName:newUser.fullName,
+        username:newUser.username,
+        profilepic:newUser.profilepic
+    })
+}else{
+    res.status(400).json({error:"Invalid user data"});
+}
+}catch(error){
+    console.log("Error in signup controoler", error.message)
+    res.status(500).json({error:error.message})
     }
 };
 export const loginUser =(req,res)=>{
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+
 import authRoutes from "./routes/authRoutes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js"
+;
 const app = express(); 
-
-dotenv.config();
 const PORT =process.env.PORT || 5000; 
+dotenv.config();
+app.use(express.json());
+app.use("/api/auth",authRoutes); 
 
 
-app.get("/",(req,res)=>{ 
+
+/*app.get("/",(req,res)=>{ 
     res.send("hello hi")
-})
+})*/
 
 /*app.get("/api/auth/signup",(req,res)=>{
 
@@ -29,5 +34,7 @@ app.get("/api/auth/logout",(req,res)=>{
     #INETEAD OF THIS WE'LL USE MIDDLELAYER
 
 })*/
-app.use("/api/auth",authRoutes)
-app.listen(PORT,()=> console.log(`server running on port ${PORT}`)); 
+
+app.listen(PORT,()=>{
+    connectToMongoDB()
+    console.log(`server running on port ${PORT}`)});
