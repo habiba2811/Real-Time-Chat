8c2138da976ad4bc57ad89cf0ccad8200498f584
habiba <habiba.hisham2811@gmail.com>
2024-08-01 18:07:31 +0200

    feat(User): add get users

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,9 @@
+import express from "express"
+import protectRoute from "../MiddleWare/protectRoute.js";
+import {getUserForSidebar} from "../Controllers/userController.js"
+
+const router = express.Router();
+
+router.get("/",protectRoute,getUserForSidebar)
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
-
 import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/authRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express(); 
@@ -16,6 +18,8 @@ app.use(cookieParser());
 
 app.use("/api/auth",authRoutes); 
 app.use("/api/messages",messageRoutes); 
+app.use("/api/users",userRoutes); 
+
 
 
 app.listen(PORT,()=>{
