a70433832a30827f808395e62c5246377a79a4af
habiba <habiba.hisham2811@gmail.com>
2024-08-01 14:52:53 +0200

    feat(auth): add signup and login functionality

--- backend/Controllers/authController.js
+++ backend/Controllers/authController.js
@@ -1,5 +1,7 @@
 import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
+
 export const signup =async(req,res)=>{
     try{
         const {fullName, username,password,confirmPassword,gender}= req.body;
@@ -22,8 +24,13 @@ export const signup =async(req,res)=>{
         gender,
         profilepic: gender === "male"? boyProfilePic : girlProfilePic
     })
-    if (newUser){await newUser.save();
-    res.status(201).json({
+    if (newUser){
+        //Genterate JWT token
+         generateTokenAndSetCookie(newUser._id,res);
+
+        await newUser.save();
+
+       res.status(201).json({
         _id : newUser._id,
         fullName:newUser.fullName,
         username:newUser.username,
@@ -37,9 +44,26 @@ export const signup =async(req,res)=>{
     res.status(500).json({error:error.message})
     }
 };
-export const loginUser =(req,res)=>{
-    res.send("login");
-    console.log("loginUser");
+export const loginUser = async(req,res)=>{
+    try {
+        const {username,password} = req.body;
+        const user = await User.findOne({username});
+        const isPasswordCorrect = await bcrypt.compare(password,user?.password || "");
+        if (!user || !isPasswordCorrect){
+            return res.status(400).json({error:"Invalid credentials"});
+        }
+        generateTokenAndSetCookie(user._id,res);
+        res.status(201).json({
+            _id : user._id,
+            fullName:user.fullName,
+            username:user.username,
+            profilepic:user.profilepic
+        })
+
+    } catch (error) {
+        console.log("Error in login controoler", error.message)
+        res.status(500).json({error:error.message})
+    }
     
 }
 
