3345175ac16596d8baf3682314400db98875025b
habiba <habiba.hisham2811@gmail.com>
2024-08-09 17:08:23 +0200

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -7,9 +8,12 @@ import messageRoutes from "./routes/messageRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
-import {app,server} from "./socket/socket.js"
+import {app,server
+} from "./socket/socket.js"
 
 const PORT =process.env.PORT || 5000; 
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -19,6 +23,12 @@ app.use(cookieParser());
 app.use("/api/auth",authRoutes); 
 app.use("/api/messages",messageRoutes); 
 app.use("/api/users",userRoutes); 
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname,"frontend", "dist","index.html"))
+})
 
 
 
